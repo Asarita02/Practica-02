a41675b7a27204722ce901f98b09d702872452e3
Asarita02 <124800321+Asarita02@users.noreply.github.com>
2023-02-14 17:15:14 -0400

    programa que resuelva sumas o diferencia de cubos
    Ejercicio 3

---
+++ Operacionesconcubos/Operacionesconcubos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacionesconcubos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bienvenido al programa Geomel que calcula la suma o diferencia de cubos"); 
+
+            Console.WriteLine("Ingrese el valor del primer cubo:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor del segundo cubo:");
+            int b = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            if (a > b)
+            {
+                resultado = (a + b) * (a - b) * (a * a + b * b + a * b);
+            }
+            else if (b > a)
+            {
+                resultado = (a + b) * (b - a) * (a * a + b * b - a * b);
+            }
+            else
+            {
+                Console.WriteLine("a y b son iguales, no se puede resolver");
+                return;
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadLine();
+
+        }
+    }
+}
