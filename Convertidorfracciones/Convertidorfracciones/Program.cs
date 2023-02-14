e7c8ab0b069838f421ab161c9e4f189c939445e7
Asarita02 <124800321+Asarita02@users.noreply.github.com>
2023-02-14 16:37:11 -0400

    Programa conversión de fracciones mixtas a fracciones comunes
    Ejercicio 0

---
+++ Convertidorfracciones/Convertidorfracciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertidorfracciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que convierte fracciones mixtas en fracciones comunes Geomel");
+
+            int numerador, denominador, entero;
+
+            Console.Write("Ingrese la fracción mixta (en el formato 'entero numerador/denominador'): ");
+            string fraccionMixta = Console.ReadLine();
+
+            string[] fraccionSplit = fraccionMixta.Split(' ', '/');
+            entero = Convert.ToInt32(fraccionSplit[0]);
+            numerador = Convert.ToInt32(fraccionSplit[1]);
+            denominador = Convert.ToInt32(fraccionSplit[2]);
+
+           
+            numerador += entero * denominador;
+
+            Console.WriteLine("La fracción mixta " + fraccionMixta + " en fracción común es: " + numerador + "/" + denominador);
+
+            Console.ReadLine();
+        }
+    }  
+    
+}
