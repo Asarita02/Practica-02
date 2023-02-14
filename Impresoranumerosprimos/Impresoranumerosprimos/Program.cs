2db89d533a6718697947dc20670c541d50b6a30b
Asarita02 <124800321+Asarita02@users.noreply.github.com>
2023-02-14 16:53:02 -0400

    Programa imprima los números primos
    Ejercicio 1

---
+++ Impresoranumerosprimos/Impresoranumerosprimos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impresoranumerosprimos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio = 2; // Número inicial
+            int fin = 20; // Número final
+
+            Console.WriteLine("Programa que imprime una lista de numeros primos Geomel");
+
+            for (int i = inicio; i <= fin; i++)
+            {
+                bool esPrimo = true;
+
+                for (int M = 2; M < i; M++)
+                {
+                    if (i % M == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+
+                if (esPrimo)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
