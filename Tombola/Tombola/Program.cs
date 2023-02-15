f39b588b7db7c07dea473564d6aff29fce038c54
Asarita02 <124800321+Asarita02@users.noreply.github.com>
2023-02-15 09:45:25 -0400

    Tombola
    Ejercicio 6

---
+++ Tombola/Tombola/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tombola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            
+            Console.WriteLine("Programa que simula una tombola");
+            Console.ReadLine();
+
+            Random random = new Random();
+
+            int numero1 = random.Next(100);
+            int numero2 = random.Next(100);
+            int numero3 = random.Next(100);
+
+            
+            Console.WriteLine("Los números generados son:");
+            Console.WriteLine(numero1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero3);
+            Console.ReadLine();
+        }
+    }
+}
